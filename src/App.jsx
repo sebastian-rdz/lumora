97bcc57da1f10a7131219a70df9247d0d8c1ae79
Sebastian Zavala <mrjchzz@gmail.com>
2026-07-26 16:00:51 -0600

    chore: scaffold vite/react/three.js

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Lumora</h1>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
