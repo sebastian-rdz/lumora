2334ce1322a314e797dc89e3f96ee50cc38aaabb
Sebastian Zavala <mrjchzz@gmail.com>
2026-07-26 16:12:05 -0600

    feat: render first seat

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import { useState } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 import './App.css'
+
+function Seat() {
+  return (
+    <group>
+      <mesh position={[0, 0.25, 0]}>
+        <boxGeometry args={[0.5, 0.1, 0.5]} />
+        <meshStandardMaterial color="#7a1620" />
+      </mesh>
+      <mesh position={[0, 0.55, 0.2]} rotation={[-0.15, 0, 0]}>
+        <boxGeometry args={[0.5, 0.6, 0.1]} />
+        <meshStandardMaterial color="#7a1620" />
+      </mesh>
+    </group>
+  )
+}
 
 function App() {
   return (
-    <>
-      <section id="center">
-        <div>
-          <h1>Lumora</h1>
-        </div>
-      </section>
-    </>
+    <div className="app">
+      <Canvas camera={{ position: [1.5, 1.5, 2.5], fov: 50 }}>
+        <color attach="background" args={['#0d0a0f']} />
+        <ambientLight intensity={0.6} />
+        <directionalLight position={[3, 4, 2]} intensity={1.2} />
+        <Seat />
+        <OrbitControls />
+      </Canvas>
+    </div>
   )
 }
 
